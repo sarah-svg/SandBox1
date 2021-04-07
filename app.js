6a87377772df5545948343c3c1185acabcbff331
sarah-svg <kjskillen@gmail.com>
2021-04-06 23:15:43 -0700

    create

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
-import {createShopLi } from './utilis.js';
+import { createShopLi, createTableRow } from './utilis.js';
 // initialize state
 const newShop = createShopLi();
+const table = createTableRow();
 // set event listeners to update state and DOM
---
+++ cart/app.js
@@ -0,0 +1,21 @@
+console.log('heu');
+import { findById, getShop} from '../localStorage.js';
+import { item } from '../products/products.js';
+import { createTableRow } from '../utilis.js';
+
+
+const button = document.getElementById('submit');
+
+const shop = getShop();
+
+const table = document.querySelector('table');
+for (let oneItem of item){
+
+    const match = findById(item, oneItem.id);
+    const tr = createTableRow(oneItem, match);
+
+    table.append(tr);
+    console.log(match,'match' ,tr);
+}
+
+console.log(item, shop);
